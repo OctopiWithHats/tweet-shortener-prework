23a30cd655e79f0be20f71903adc870312615e9a
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-22 21:52:59 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,5 @@
-# Write your code here.
-
 def dictionary
-  words = {
+  convert = {
    "hello" => "hi",
    "to" => "2",
    "two" => "2",
@@ -15,13 +13,40 @@ def dictionary
 }
 end
 
-def word_substitutor(tweet)
-  
-  tweet.split(" ").map do |word|
-    if dictionary.keys.include?(word.downcase)
-      word = dictionary[word.downcase]
+def word_substituter(tweet)
+  # replaces long words with their expected short form
+
+    tweet.split(" ").map do |word|
+      if dictionary.keys.include?(word.downcase)
+        word = dictionary[word.downcase]
+      else
+        word
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweet)
+  tweet.map do |phrase|
+    puts word_substituter(phrase)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)
+    elsif phrase.length <= 140
+      phrase
+    end
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  tweet.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)[0..140] + "..."
     else
-      word
+      phrase
     end
   end.join(" ")
 end
